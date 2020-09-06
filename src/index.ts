36fe02845b43f9096c249615c275cea555874ef4
Boshen <boshenc@gmail.com>
2020-09-06 18:46:05 +0800

    prepare release

--- src/index.ts
+++ src/index.ts
@@ -1,98 +1,96 @@
 export default class FaviconBadge {
-  r = Math.ceil(window.devicePixelRatio) || 1;
-  size = 16 * this.r;
-  width = 7
-  height = 9
-  color = '#ffffff'
-  background = 'red'
-  font = 10 * this.r + 'px arial'
-  crossOrigin = false
+  private r = Math.ceil(window.devicePixelRatio) || 1
+  private size = 16 * this.r
+  private width = 7
+  private height = 9
+  private background = 'red'
+  private font = 10 * this.r + 'px arial'
+  private crossOrigin = false
 
-  canvas: HTMLCanvasElement = this.getCanvas()
-  currentFavicon: string | null = null
-  originalFavicon: string | null = null
+  private canvas: HTMLCanvasElement = this.getCanvas()
+  private currentFavicon: string | null = null
+  private originalFavicon: string | null = null
 
-  agent = navigator.userAgent.toLowerCase()
+  private agent = navigator.userAgent.toLowerCase()
 
-  browser = {
+  private browser = {
     ie: this.ua('trident'),
     chrome: this.ua('chrome'),
     webkit: this.ua('chrome') || this.ua('safari'),
     safari: this.ua('safari') && !this.ua('chrome'),
-    mozilla: this.ua('mozilla') && !this.ua('chrome') && !this.ua('safari')
-  };
+    mozilla: this.ua('mozilla') && !this.ua('chrome') && !this.ua('safari'),
+  }
 
-  private ua(browser: string ) {
-    return this.agent.indexOf(browser) !== -1;
+  private ua(browser: string) {
+    return this.agent.indexOf(browser) !== -1
   }
 
   private getCanvas() {
     if (this.canvas) {
       return this.canvas
     }
-    const canvas = document.createElement("canvas");
-    canvas.width = this.size;
-    canvas.height = this.size;
+    const canvas = document.createElement('canvas')
+    canvas.width = this.size
+    canvas.height = this.size
     return canvas
-  };
+  }
 
   private getCurrentFavicon() {
     if (!this.originalFavicon || !this.currentFavicon) {
-      const tag = this.getFaviconTag();
-      this.currentFavicon = tag ? tag.getAttribute('href') : '/favicon.ico';
+      const tag = this.getFaviconTag()
+      this.currentFavicon = tag ? tag.getAttribute('href') : '/favicon.ico'
       if (!this.originalFavicon) {
-        this.originalFavicon = this.currentFavicon;
+        this.originalFavicon = this.currentFavicon
       }
     }
-    return this.currentFavicon;
-  };
+    return this.currentFavicon
+  }
 
   private getFaviconTag() {
-    const links = document.getElementsByTagName('link');
-    for(let i=0; i < links.length; i++) {
+    const links = document.getElementsByTagName('link')
+    for (let i = 0; i < links.length; i++) {
       if ((links[i].getAttribute('rel') || '').match(/\bicon\b/i)) {
-        return links[i];
+        return links[i]
       }
     }
-    return false;
-  };
+    return false
+  }
 
-
-  removeFaviconTag() {
-    const links = document.getElementsByTagName('link');
-    for(let i=0, len=links.length; i < len; i++) {
-      const exists = (typeof(links[i]) !== 'undefined');
+  private removeFaviconTag() {
+    const links = document.getElementsByTagName('link')
+    for (let i = 0, len = links.length; i < len; i++) {
+      const exists = typeof links[i] !== 'undefined'
       if (exists && (links[i].getAttribute('rel') || '').match(/\bicon\b/i)) {
-        links[i].parentNode!.removeChild(links[i]);
+        links[i].parentNode!.removeChild(links[i])
       }
     }
-  };
+  }
 
   private setFaviconTag(url: string) {
-    if(url){
-      this.removeFaviconTag();
-      const link = document.createElement('link');
-      link.type = 'image/x-icon';
-      link.rel = 'icon';
-      link.href = url;
-      document.getElementsByTagName('head')[0].appendChild(link);
+    if (url) {
+      this.removeFaviconTag()
+      const link = document.createElement('link')
+      link.type = 'image/x-icon'
+      link.rel = 'icon'
+      link.href = url
+      document.getElementsByTagName('head')[0].appendChild(link)
     }
-  };
+  }
 
   private refreshFavicon() {
     if (!this.canvas) {
       return
     }
-    this.setFaviconTag(this.canvas.toDataURL());
-  };
+    this.setFaviconTag(this.canvas.toDataURL())
+  }
 
   private drawBubble(label: string, color: string) {
     // bubble needs to be larger for double digits
-    const context = this.canvas.getContext("2d")!;
-    const len = label.length-1;
+    const context = this.canvas.getContext('2d')!
+    const len = label.length - 1
     const r = this.r
 
-    const width = this.width * r + (6 * r * len)
+    const width = this.width * r + 6 * r * len
     const height = this.height * r
 
     const top = this.size - height
@@ -102,60 +100,58 @@ export default class FaviconBadge {
     const radius = 2 * r
 
     // webkit seems to render fonts lighter than firefox
-    context.font = (this.browser.webkit ? 'bold ' : '') + this.font;
-    context.fillStyle = this.background;
-    context.strokeStyle = this.background;
-    context.lineWidth = r;
+    context.font = (this.browser.webkit ? 'bold ' : '') + this.font
+    context.fillStyle = this.background
+    context.strokeStyle = this.background
+    context.lineWidth = r
 
     // bubble
-    context.beginPath();
-    context.moveTo(left + radius, top);
-    context.quadraticCurveTo(left, top, left, top + radius);
-    context.lineTo(left, bottom - radius);
-    context.quadraticCurveTo(left, bottom, left + radius, bottom);
-    context.lineTo(right - radius, bottom);
-    context.quadraticCurveTo(right, bottom, right, bottom - radius);
-    context.lineTo(right, top + radius);
-    context.quadraticCurveTo(right, top, right - radius, top);
-    context.closePath();
-    context.fill();
+    context.beginPath()
+    context.moveTo(left + radius, top)
+    context.quadraticCurveTo(left, top, left, top + radius)
+    context.lineTo(left, bottom - radius)
+    context.quadraticCurveTo(left, bottom, left + radius, bottom)
+    context.lineTo(right - radius, bottom)
+    context.quadraticCurveTo(right, bottom, right, bottom - radius)
+    context.lineTo(right, top + radius)
+    context.quadraticCurveTo(right, top, right - radius, top)
+    context.closePath()
+    context.fill()
 
     // bottom shadow
-    context.beginPath();
-    context.strokeStyle = "rgba(0,0,0,0.3)";
-    context.moveTo(left + radius / 2.0, bottom);
-    context.lineTo(right - radius / 2.0, bottom);
-    context.stroke();
+    context.beginPath()
+    context.strokeStyle = 'rgba(0,0,0,0.3)'
+    context.moveTo(left + radius / 2.0, bottom)
+    context.lineTo(right - radius / 2.0, bottom)
+    context.stroke()
 
     // label
-    context.fillStyle = color;
-    context.textAlign = "right";
-    context.textBaseline = "top";
+    context.fillStyle = color
+    context.textAlign = 'right'
+    context.textBaseline = 'top'
 
     // unfortunately webkit/mozilla are a pixel different in text positioning
-    context.fillText(label, r === 2 ? 29 : 15, this.browser.mozilla ? 7*r : 6*r);
-  };
-
+    context.fillText(label, r === 2 ? 29 : 15, this.browser.mozilla ? 7 * r : 6 * r)
+  }
 
   public draw(label: string, color = '#ffffff') {
-    const context = this.canvas.getContext("2d")!;
+    const context = this.canvas.getContext('2d')!
 
-    const faviconImage = document.createElement('img');
+    const faviconImage = document.createElement('img')
     faviconImage.onload = () => {
-      context.clearRect(0, 0, this.size, this.size);
-      context.drawImage(faviconImage, 0, 0, faviconImage.width, faviconImage.height, 0, 0, this.size, this.size);
+      context.clearRect(0, 0, this.size, this.size)
+      context.drawImage(faviconImage, 0, 0, faviconImage.width, faviconImage.height, 0, 0, this.size, this.size)
       if (label.length > 0) {
-        this.drawBubble(label, color);
+        this.drawBubble(label, color)
       }
-      this.refreshFavicon();
-    };
+      this.refreshFavicon()
+    }
 
-    const src = this.getCurrentFavicon() || '';
+    const src = this.getCurrentFavicon() || ''
     if (!src.match(/^data/) && this.crossOrigin) {
-      faviconImage.crossOrigin = 'anonymous';
+      faviconImage.crossOrigin = 'anonymous'
     }
     faviconImage.src = src
     faviconImage.onload(null as any)
   }
-
 }
